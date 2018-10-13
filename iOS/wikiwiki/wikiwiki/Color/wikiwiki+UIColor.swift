cb39ee9c7288fc7868075a58e3977d0f317adce0
pblin <pblin@umich.edu>
2018-10-13 16:56:39 -0400

    colors added

--- iOS/wikiwiki/wikiwiki/Color/wikiwiki+UIColor.swift
+++ iOS/wikiwiki/wikiwiki/Color/wikiwiki+UIColor.swift
@@ -10,28 +10,20 @@ import UIKit
 
 extension UIColor {
     public enum wikiwiki {
-        case red, magenta, green, blue, orange, yellow, purple, pink, gray
+        case red, green, blue, orange, purple
         
         public func color() -> UIColor {
             switch self {
             case .red:
-                return UIColor(red:0.93, green:0.26, blue:0.26, alpha:1.0)
-            case .magenta:
-                return UIColor(red:0.93, green:0.26, blue:0.26, alpha:1.0)
-            case .green:
-                return UIColor(red:0.44, green:0.79, blue:0.58, alpha:1.0)
-            case .blue:
-                return UIColor(red:0.52, green:0.74, blue:0.85, alpha:1.0)
+                return UIColor(red:0.89, green:0.34, blue:0.18, alpha:1.0)
             case .orange:
-                return UIColor(red:0.95, green:0.47, blue:0.28, alpha:1.0)
-            case .yellow:
-                return UIColor(red:0.96, green:0.68, blue:0.18, alpha:1.0)
+                return UIColor(red:1.00, green:0.65, blue:0.00, alpha:1.0)
+            case .green:
+                return UIColor(red:0.36, green:0.99, blue:0.80, alpha:1.0)
+            case .blue:
+                return UIColor(red:0.29, green:0.70, blue:0.99, alpha:1.0)
             case .purple:
-                return UIColor(red:0.54, green:0.24, blue:0.82, alpha:1.0)
-            case .pink:
-                return UIColor(red:0.95, green:0.59, blue:0.84, alpha:1.0)
-            case .gray:
-                return UIColor(red:0.60, green:0.60, blue:0.60, alpha:1.0)
+                return UIColor(red:0.70, green:0.53, blue:0.92, alpha:1.0)
             }
         }
     }
