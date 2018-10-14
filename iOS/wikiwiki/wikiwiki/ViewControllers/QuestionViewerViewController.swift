04220cb52013d0b29f00f0d20130804782c1db1d
Brian Lin <brian.poan.lin@gmail.com>
2018-10-14 04:28:33 -0400

    calling vc works;

--- iOS/wikiwiki/wikiwiki/ViewControllers/QuestionViewerViewController.swift
+++ iOS/wikiwiki/wikiwiki/ViewControllers/QuestionViewerViewController.swift
@@ -10,23 +10,31 @@ import Foundation
 import UIKit
 
 public class QuestionViewerViewController: UIViewController {
+    var questionViewerView: CommunityQuestionCardView
     private var questions: [Question]
     
     public init(questions: [Question]) {
         self.questions = questions
+        self.questionViewerView = CommunityQuestionCardView(question: questions[0], color: UIColor.wikiwiki.orange.color())
         super.init(nibName: nil, bundle: nil)
     }
     
     public override func viewDidLoad() {
         view.backgroundColor = .white
+        setupViews()
+        setupConstraints()
     }
     
     public func setupViews() {
-        
+        view.addSubview(questionViewerView)
     }
     
     public func setupConstraints() {
-        
+        questionViewerView.translatesAutoresizingMaskIntoConstraints = false
+        questionViewerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -80).isActive = true
+        questionViewerView.heightAnchor.constraint(equalToConstant: 400).isActive = true
+        questionViewerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        questionViewerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
