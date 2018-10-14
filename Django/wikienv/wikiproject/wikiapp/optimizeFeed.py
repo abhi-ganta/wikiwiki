d0bd72b202e50daa0ab069afa70c534770f1d126
aganta <aganta@umich.edu>
2018-10-14 03:51:17 -0400

    acusn

--- Django/wikienv/wikiproject/wikiapp/optimizeFeed.py
+++ Django/wikienv/wikiproject/wikiapp/optimizeFeed.py
@@ -13,12 +13,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 
-def jobPredict(X_train, y_train, X_predict):
+def feedOptimize(X_train, y_train, X_predict):
     model = LogisticRegression()
 
     # model = RandomForestClassifier(n_estimators = 400, max_depth = 70, max_features = 'auto')
 
-    model = GradientBoostingClassifier(n_estimators = 1000, max_depth = 3, learning_rate = 0.1)
+    # model = GradientBoostingClassifier(n_estimators = 1000, max_depth = 3, learning_rate = 0.1)
 
     X_tr, X_te, y_tr, y_te = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
@@ -34,33 +34,42 @@ def jobPredict(X_train, y_train, X_predict):
 def optimizeFeedApi(requestJson):
 
     oldData = requestJson['old']
+    newData = requestJson['new']
 
-    X_cols = ['aLen', 'c0', 'c1', 'c2', 'c3', 'c4', 'qLen']
+    X_cols = ['aLen', 'c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'qLen']
 
-    old = pd.DataFrame(oldData)
+    trainData = pd.DataFrame(oldData)
+    testData = pd.DataFrame(newData)
 
-    X = old[X_cols]
-    y = old['voted']
+    X_train = trainData[X_cols]
+    y_train = trainData['voted']
+    X_predict = testData[X_cols]
+    predict_ids = testData['poll_id']
 
-    # newthing = newthing.transpose()
+    predict_ids = pd.DataFrame(predict_ids)
 
-    print (X)
-    print (y)
+    feedProb, accuracy = feedOptimize(X_train, y_train, X_predict)
 
-    # trainData = requestJson['old-data']
-    # testData = requestJson['to-predict']
-    #
-    # trainData = read_json(trainData)
-    # testData = read_json(testData)
-    #
-    # X_cols = ['']
-    #
-    # X_train = trainData[X_cols]
-    # y_train = trainData['result']
-    #
-    # X_predict = testData[X_cols]
-    # jobMatchProb, accuracy = jobPredict(X_train, y_train, X_predict)
+    feedProb = pd.DataFrame(feedProb)
 
+    prediction_bind = predict_ids.join(feedProb)
 
+    prediction_bind.columns = ['predict_id', 'prob']
 
-    return requestJson
+    print(prediction_bind)
+
+    returnResponse = {
+        'accuracy': accuracy,
+        'predictions': []
+    }
+
+    predictionReturn = returnResponse['predictions']
+
+    for index, x in prediction_bind.iterrows():
+        oneProb = {
+            'poll_id': x['predict_id'],
+            'probability': x['prob']
+        }
+        predictionReturn.append(oneProb)
+
+    return returnResponse
