0b59296b6c1c8a7cadb5d266a5ceaba188e38ed0
aganta <aganta@umich.edu>
2018-10-14 02:34:01 -0400

    jajaja

--- Django/wikienv/wikiproject/wikiapp/optimizeFeed.py
+++ Django/wikienv/wikiproject/wikiapp/optimizeFeed.py
@@ -13,48 +13,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 
-def read_mongo(connection, collection, recruiter_id, student_id):
-    # Connect to MongoDB
-    conn = MongoClient(connection)
-
-    db = conn['reko']
-
-    # Make a query to the specific DB and Collection
-    collection = db.users
-
-    # Expand the cursor and construct the DataFrame
-    data = pd.DataFrame(list(collection.find({'user': recruiter_id})))
-    studentData = list(collection.find({'user': student_id}))
-
-    testData = []
-
-    testData.append(studentData[0]['we'])
-    testData.append(studentData[0]['ed'])
-    testData.append(studentData[0]['sk'])
-    testData.append(studentData[0]['aw'])
-    testData.append(studentData[0]['vl'])
-    testData.append(studentData[0]['cw'])
-    testData.append(studentData[0]['ot'])
-
-    trainData = []
-    imp = data['imp']
-    for x in imp:
-        for y in x:
-            trainData.append(y['ratings'])
-    trainData = pd.DataFrame(trainData, columns = ['we', 'ed', 'sk', 'aw', 'vl', 'cw', 'ot', 'result'])
-    testData = pd.DataFrame([testData], columns = ['we', 'ed', 'sk', 'aw', 'vl', 'cw', 'ot'])
-
-    print(trainData)
-    print(testData)
-
-    conn.close()
-
-    return trainData, testData
-
 def jobPredict(X_train, y_train, X_predict):
     model = LogisticRegression()
 
-    # model = RandomForestClassifier(n_estimators = 1000)
+    # model = RandomForestClassifier(n_estimators = 400, max_depth = 70, max_features = 'auto')
+
+    model = GradientBoostingClassifier(n_estimators = 1000, max_depth = 3, learning_rate = 0.1)
 
     X_tr, X_te, y_tr, y_te = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
@@ -67,68 +31,37 @@ def jobPredict(X_train, y_train, X_predict):
 
     return prediction_prob[:,1], accuracy
 
-def jobMatchApi(requestJson):
-    connection = 'mongodb+srv://poppro:reko123@reko-no8a0.gcp.mongodb.net/'
-    collection = 'users'
-    print (requestJson)
-    student_id = requestJson['student_id']
-    recruiter_id = requestJson['recruiter_id']
-
-    print (student_id)
-    print (recruiter_id)
-
-    trainData, testData = read_mongo(connection, collection, recruiter_id, student_id)
-
-    X_cols = ['we', 'ed', 'sk', 'aw', 'vl', 'cw', 'ot']
-
-    X_train = trainData[X_cols]
-    y_train = trainData['result']
-
-    X_predict = testData[X_cols]
-    jobMatchProb, accuracy = jobPredict(X_train, y_train, X_predict)
-
-    we_a = X_train['we'].mean()
-    ed_a = X_train['ed'].mean()
-    sk_a = X_train['sk'].mean()
-    aw_a = X_train['aw'].mean()
-    vl_a = X_train['vl'].mean()
-    cw_a = X_train['cw'].mean()
-    ot_a = X_train['ot'].mean()
-
-    returnJson = {
-        'student_id': student_id,
-        'student_stats': {
-            'we':{
-                's': X_predict['we'][0],
-                'a': we_a
-            },
-            'ed':{
-                's':X_predict['ed'][0],
-                'a': ed_a
-            },
-            'sk':{
-                's': X_predict['sk'][0],
-                'a': sk_a
-            },
-            'aw':{
-                's': X_predict['aw'][0],
-                'a': aw_a
-            },
-            'vl':{
-                's': X_predict['vl'][0],
-                'a': vl_a
-            },
-            'cw':{
-                's': X_predict['cw'][0],
-                'a': cw_a
-            },
-            'ot':{
-                's': X_predict['ot'][0],
-                'a': ot_a
-            },
-            'jobMatchProb': jobMatchProb[0] * 100,
-        },
-        'accuracy': accuracy * 100
+def optimizeFeedApi(requestJson):
+    testString = {
+        'aLen': 28,
+        'c0': 1,
+        'c1': 0,
+        'c2': 0,
+        'c3': 1,
+        'c4': 0,
+        'poll_id': '-LOjXU_uxcj7I_bT23Q_',
+        'qLen': 23,
+        'voted': 1
     }
 
-    return returnJson
+    print(requestJson)
+
+
+
+    # trainData = requestJson['old-data']
+    # testData = requestJson['to-predict']
+    #
+    # trainData = read_json(trainData)
+    # testData = read_json(testData)
+    #
+    # X_cols = ['']
+    #
+    # X_train = trainData[X_cols]
+    # y_train = trainData['result']
+    #
+    # X_predict = testData[X_cols]
+    # jobMatchProb, accuracy = jobPredict(X_train, y_train, X_predict)
+
+
+
+    return requestJson
